99185916e65aa571959f178c155b866ff5965624
Clarenskyy <150917878+Clarenskyy@users.noreply.github.com>
2024-03-24 13:48:23 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username: ");
+            String username = Console.ReadLine();
+
+            Console.WriteLine("Enter password: ");
+            String password = Console.ReadLine();
+            if (username == "Admin" && password == "Admin1234!")
+            {
+                Console.WriteLine("Login Successful!");
+            }
+            else
+            {
+                Console.WriteLine("\n\nIncorrect Password or Username");
+                Console.WriteLine("\t3 attemps left");
+
+                Console.WriteLine("Enter username: ");
+                username = Console.ReadLine();
+
+                Console.WriteLine("Enter password: ");
+                password = Console.ReadLine();
+                if (username == "Admin" && password == "Admin1234!")
+                {
+                    Console.WriteLine("Login Successful!");
+                }
+                else
+                {
+                    Console.WriteLine("\n\nIncorrect Password or Username");
+                    Console.WriteLine("\t2 attemps left");
+
+                    Console.WriteLine("Enter username: ");
+                    username = Console.ReadLine();
+
+                    Console.WriteLine("Enter password: ");
+                    password = Console.ReadLine();
+                    if (username == "Admin" && password == "Admin1234!")
+                    {
+                        Console.WriteLine("Login Successful!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\nIncorrect Password or Username");
+                        Console.WriteLine("\t1 attemps left");
+
+                        Console.WriteLine("Enter username: ");
+                        username = Console.ReadLine();
+
+                        Console.WriteLine("Enter password: ");
+                        password = Console.ReadLine();
+                        if (username == "Admin" && password == "Admin1234!")
+                        {
+                            Console.WriteLine("Login Successful!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n\nIncorrect Password or Username");
+                            Console.WriteLine(" Account lock for a few minutes, try again later. ");
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
